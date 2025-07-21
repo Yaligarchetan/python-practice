f32bb6ad4c1a93f41298067d60cf805a6cd60d8b
Chetan Yaligar <122506313+Yaligarchetan@users.noreply.github.com>
2025-07-21 14:14:18 +0530

    FastAPI basics🚀

---
+++ Basics/FastAPI.py
@@ -0,0 +1,10 @@
+# learning fastapi basics 
+# Task	Command
+# Create a virtual environment in python 
+'''
+Create env -	python -m venv venv
+Activate (Windows)-	venv\Scripts\activate
+Install packages-	pip install ...
+Save dependencies -	pip freeze > requirements.txt
+Exit env -	deactivate
+'''
